25107c2b670f9882b4bfa7a9d989f0b966b64b5a
GolovkoStepan <stepagolovko1993@gmail.com>
2020-09-18 23:12:20 +0500

    Добавление флеш сообщений об ошибках.

--- stepik_vacancies/urls.py
+++ stepik_vacancies/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from stepik_vacancies.views import MainView
+from stepik_vacancies.views import MainView, view_404, view_500
+
+handler404 = view_404
+handler500 = view_500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- stepik_vacancies/views.py
+++ stepik_vacancies/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views import View
 
 from companies.models import Company
 from vacancies.models import Specialty
+
+
+def view_404(request, *args, **kwargs):
+    messages.add_message(request, messages.INFO, 'Запрашиваемая вами страница не была найдена')
+    return redirect('/')
+
+
+def view_500(request, *args, **kwargs):
+    messages.add_message(request, messages.INFO, 'При обработке вашего запроса произошла ошибка')
+    return redirect('/')
 
 
 class MainView(View):
