24c389910a9f8af62160806bfe0fb49031f3d931
GolovkoStepan <stepagolovko1993@gmail.com>
2020-09-26 14:13:21 +0500

    Разработка функционала добавления резюме и поиска.

--- stepik_vacancies/urls.py
+++ stepik_vacancies/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 
-from stepik_vacancies.views import MainView, view_404, view_500
+from stepik_vacancies.views import MainView, view_404, view_500, UserSignupView, UserLoginView, UserResume, \
+    UserProfileView
+from vacancies.views import SearchView
 
 handler404 = view_404
 handler500 = view_500
@@ -10,5 +13,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainView.as_view()),
     path('vacancies/', include('vacancies.urls')),
-    path('companies/', include('companies.urls'))
+    path('companies/', include('companies.urls')),
+    path('login', UserLoginView.as_view()),
+    path('logout', LogoutView.as_view()),
+    path('signup', UserSignupView.as_view()),
+    path('profile', UserProfileView.as_view()),
+    path('my_resume', UserResume.as_view()),
+    path('search', SearchView.as_view())
 ]
